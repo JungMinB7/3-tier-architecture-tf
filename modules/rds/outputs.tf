e80843355f4149f9aa332c8665171686a0ccec73
이정민 <ljm@ijeongmin-ui-MacBookPro.local>
2025-09-02 16:02:52 +0900

    [FEAT] rds 모듈: 출력 파일 추가
    생성된 DB 인스턴스의 엔드포인트, 포트, ARN 및 ID를 출력합니다.

---
+++ modules/rds/outputs.tf
@@ -0,0 +1,4 @@
+output "endpoint" { value = aws_db_instance.db_instance.address }
+output "port"     { value = aws_db_instance.db_instance.port }
+output "arn"      { value = aws_db_instance.db_instance.arn }
+output "id"       { value = aws_db_instance.db_instance.id }
