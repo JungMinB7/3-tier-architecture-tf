1a6d571b9fa80a4459e4b703f8cebae0282eb08a
이정민 <ljm@ijeongmin-ui-MacBookPro.local>
2025-09-02 15:02:34 +0900

    [REFACTOR] vpc 모듈: IGW ID 출력 추가

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,3 +1,4 @@
 output "id"         { value = aws_vpc.aws_vpc.id }
 output "arn"        { value = aws_vpc.aws_vpc.arn }
 output "cidr_block" { value = aws_vpc.aws_vpc.cidr_block }
+output "igw_id"  { value = aws_internet_gateway.internet_gateway.id }
