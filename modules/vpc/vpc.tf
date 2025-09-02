f0dd2af39d35b838360bc36c8d5a57939bfc2b6f
이정민 <ljm@ijeongmin-ui-MacBookPro.local>
2025-09-02 15:02:24 +0900

    [REFACTOR] vpc 모듈: IGW 리소스 추가

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -3,5 +3,10 @@ resource "aws_vpc" "aws_vpc" {
   enable_dns_support   = var.enable_dns_support
   enable_dns_hostnames = var.enable_dns_hostnames
 
-  tags = merge(var.tags, { Name = var.aws_vpc_name })
+  tags = merge(var.tags, { Name = "${var.name}-vpc" })
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.aws_vpc.id
+  tags   = merge(var.tags, { Name = "${var.name}-igw" })
 }
